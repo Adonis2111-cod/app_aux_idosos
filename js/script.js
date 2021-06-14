91e8906fe5587651bf6f6855e5a28161f5962bff
Adonis hernandes auxilio <adonis2111@outlook.com>
2021-06-14 12:40:51 -0300

    Novas imagens

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function mudar_slide(){
-    document.getElementById("setas").src = "https://lh3.googleusercontent.com/proxy/-8DET7u8F5VfYa1AfTiSehI_Txvlsu7qedmBNC8qW3yjICBb05EmNM0a1AFXF3gS4roQJdvDMqI9EKX2dDZOPyExkkepGR6J3EMoEJjE4A-o"
+    document.getElementById("setas").src = "http://alzheimer360.com/wp-content/uploads/2017/04/remedios-pessoa-Alzheimer-840x430.jpg"
     document.getElementById("words").innerHTML="Sed scelerisque porttitor enim, sed dignissim felis laoreet vel. Aliquam posuere augue non risus fermentum, ut congue nunc consequat. Morbi vitae urna egestas, hendrerit velit vitae, interdum arcu. Nulla a dui tellus. In porttitor non arcu quis luctus. Vestibulum condimentum massa orci, at tincidunt purus faucibus a. Quisque luctus, lectus accumsan tempor aliquet, est nulla pretium mi, ac sodales nibh urna ac velit. Aliquam euismod sem eu dignissim pellentesque. Nam ac tortor a diam ultricies laoreet in id ex."
 }
 
 function mudar_slide2(){
-    document.getElementById("setas").src = "https://www.tempoeequilibrio.com.br/wp-content/uploads/2017/04/A-pessoa-com-Alzheimer-n%C3%A3o-quer-tomar-os-rem%C3%A9dios..jpg"
+    document.getElementById("setas").src = "https://portalamigodoidoso.com.br/wp-content/uploads/2016/02/Medication-Supervision-1024x682.jpg"
     document.getElementById("words").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et leo non leo iaculis cursus vel nec magna. Proin interdum metus erat, eu hendrerit enim hendrerit in. Mauris quis augue sit amet velit varius scelerisque. Duis ut nibh porttitor, ultrices arcu at, tristique diam. Sed non hendrerit enim, vel placerat libero. Aliquam blandit ipsum massa, ut commodo libero facilisis sed. Proin ultricies luctus sapien ut volutpat. Nam libero ipsum, consequat at aliquam eu, tristique eu massa."
 }
