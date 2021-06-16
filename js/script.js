a988f604605543f8c503edc2ad46556462313263
Adonis hernandes auxilio <adonis2111@outlook.com>
2021-06-16 16:24:53 -0300

    Atualizacao final

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 function mudar_slide(){
     document.getElementById("setas").src = "http://alzheimer360.com/wp-content/uploads/2017/04/remedios-pessoa-Alzheimer-840x430.jpg"
-    document.getElementById("words").innerHTML="Sed scelerisque porttitor enim, sed dignissim felis laoreet vel. Aliquam posuere augue non risus fermentum, ut congue nunc consequat. Morbi vitae urna egestas, hendrerit velit vitae, interdum arcu. Nulla a dui tellus. In porttitor non arcu quis luctus. Vestibulum condimentum massa orci, at tincidunt purus faucibus a. Quisque luctus, lectus accumsan tempor aliquet, est nulla pretium mi, ac sodales nibh urna ac velit. Aliquam euismod sem eu dignissim pellentesque. Nam ac tortor a diam ultricies laoreet in id ex."
+    document.getElementById("words").innerHTML="Quase todos os medicamentos podem seguir esse calendário, que ajuda os pacientes a agrupar as pílulas para que não seja necessário tomar as doses frequentemente, pular algumas ou desistir totalmente. As melhoras mais notáveis aconteceram entre pacientes com maior risco de não adesão aos remédios: os menos escolarizados e com rotinas complexas."
 }
 
 function mudar_slide2(){
     document.getElementById("setas").src = "https://portalamigodoidoso.com.br/wp-content/uploads/2016/02/Medication-Supervision-1024x682.jpg"
-    document.getElementById("words").innerHTML="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et leo non leo iaculis cursus vel nec magna. Proin interdum metus erat, eu hendrerit enim hendrerit in. Mauris quis augue sit amet velit varius scelerisque. Duis ut nibh porttitor, ultrices arcu at, tristique diam. Sed non hendrerit enim, vel placerat libero. Aliquam blandit ipsum massa, ut commodo libero facilisis sed. Proin ultricies luctus sapien ut volutpat. Nam libero ipsum, consequat at aliquam eu, tristique eu massa."
+    document.getElementById("words").innerHTML="A adesão aos remédios, uma ampla preocupação de saúde pública, é um problema especial para pessoas mais velhas. Eles tomam mais remédios do que os pacientes mais novos – não é incomum que tenham sete receitas. Ainda assim os estudos têm mostrado repetidamente que quanto mais vezes por dia você tem que usar remédios, menor a sua adesão."
+}
+
+function mostrarInstrucao() {
+    document.getElementById("form-instrucao").style.display = "block";
+    document.getElementById("desenho").style.display = "none";
+}
+
+function mostrarAplicativo() {
+    document.getElementById("form-instrucao").style.display= "none";
+    document.getElementById("desenho").style.display = "block";
 }
